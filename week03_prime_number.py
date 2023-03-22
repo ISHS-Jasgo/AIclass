fc4d53ba2f566cffb50047d32ff35f7818fb8614
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2023-03-22 20:52:22 +0900

    v0.6

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -5,6 +5,10 @@ for i in range(2, number):
     if number % i == 0:
         is_prime = False
         break
+
+# prime numbers are greater than 1
+if number <= 1:
+    is_prime = False
 # print result
 if is_prime:
     print(f"{number} is prime")
